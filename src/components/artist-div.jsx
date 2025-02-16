d693006a153105a8f05d478f11c6ef08a4e31999
meigemn <carloscc9297@gmail.com>
2025-02-16 17:16:56 +0100

    creado esqueleto de componente artista

---
+++ src/components/artist-div.jsx
@@ -0,0 +1,7 @@
+export default function ArtistDiv() {
+    return (
+        <div className="w-[10vw] h-[10vw] mx-auto mt-4 rounded-full overflow-hidden transform hover:scale-100 transition-transform duration-300">
+            <img src="/src/assets/logo_sons_of_aguirre.jpg" alt="logo" className="w-full h-full object-cover" />
+        </div>
+    );
+}
--- src/components/artists-feed.jsx
+++ src/components/artists-feed.jsx
@@ -1,7 +1,18 @@
+import ArtistDiv from "./artist-div";
+
 export default function ArtistsFeed() {
     return (
-        <div className="flex  bg-gray-100 w-[80vw] h-[20vh]">
+        <div className="flex flex-col mt-   bg-gray-100 w-[80vw] h-[20vh] items-center ">
             <h1>Feed de artistas</h1>
+            <div className="flex   bg-gray-100 w-[80vw] h-[20vh] items-center ">
+            
+            <ArtistDiv />
+            <ArtistDiv />
+            <ArtistDiv />
+            <ArtistDiv />
+            <ArtistDiv />
+            </div>
+            
         </div>
     );
 }
