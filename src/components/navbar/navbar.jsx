ad3ef8102a7dc12c555e8112121cdf1562faa57a
meigemn <carloscc9297@gmail.com>
2025-02-21 17:08:56 +0100

    navbar

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlassDollar } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 
 const Navbar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -12,14 +16,9 @@ const Navbar = () => {
                         href="#"
                         className="flex-grow-0 flex-shrink-0 relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-gray-700"
                     >
-                        Tailwind
+                        Logo
                     </a>
-                    <a
-                        href="#"
-                        className="flex-grow-0 flex-shrink-0 relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-gray-700"
-                    >
-                        Css
-                    </a>
+
                 </div>
 
                 {/* Botón hamburguesa (mobile) */}
@@ -52,25 +51,35 @@ const Navbar = () => {
                 className={`${isOpen ? 'absolute top-full left-0 right-0 bg-gray-800' : 'hidden'
                     } lg:flex lg:items-stretch lg:flex-shrink-0 lg:flex-grow`}
             >
-                <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
+                <div className="lg:flex lg:items-stretch lg:justify-end ml-auto flex flex-col lg:flex-row">
+                    {/* Search Input */}
+                    <div className="relative flex-grow-0 flex-shrink-0 py-2 px-4 w-full lg:w-auto mr-20">
+                        <div className="relative">
+                            <input
+                                type="text"
+                                placeholder="Search..."
+                                className="w-full py-1 pl-8 pr-2  text-gray-700 bg-white border border-gray-300 rounded focus:outline-none focus:border-gray-500"
+                            />
+                            <div className="absolute inset-y-0 left-0 pl-2 flex items-center pointer-events-none">
+                                <FontAwesomeIcon icon={faMagnifyingGlassDollar} className="text-gray-400" />
+                            </div>
+                        </div>
+                    </div>
                     <a
                         href="#"
-                        className="flex-grow-0 flex-shrink-0 relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-gray-700"
+                        className="flex-grow-0 flex-shrink-0 relative py-2 px-4 mr-2 rounded-md leading-normal text-white no-underline flex items-center hover:bg-[#50C878]"
                     >
-                        Item 1
+                        Iniciar Sesión
+                        <FontAwesomeIcon icon={faUser} className="text-gray-400 m-2" />
+
                     </a>
-                    <a
-                        href="#"
-                        className="flex-grow-0 flex-shrink-0 relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-gray-700"
-                    >
-                        Item 2
-                    </a>
-                    <a
+                  {/*   <a
                         href="#"
                         className="flex-grow-0 flex-shrink-0 relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-gray-700"
                     >
                         Item 3
-                    </a>
+                    </a> */}
+                    
                 </div>
             </div>
         </nav>
