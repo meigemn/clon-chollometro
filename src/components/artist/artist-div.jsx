41fb4c3fdcbc1e23d09a9b32c72881cb1f3a82ab
meigemn <carloscc9297@gmail.com>
2025-02-21 16:19:11 +0100

    cambio de proyecto

--- src/components/artist/artist-div.jsx
+++
@@ -1,7 +0,0 @@
-export default function ArtistDiv() {
-    return (
-        <div className="w-[80px] h-[80px] mx-auto mt-4 rounded-full overflow-hidden transform transition-transform duration-300 hover:scale-110">
-            <img src="/src/assets/logo_sons_of_aguirre.jpg" alt="logo" className="w-full h-full object-cover" />
-        </div>
-    );
-}
--- src/components/radio/radio-div.jsx
+++
@@ -1,7 +0,0 @@
-export default function RadioDiv() {
-    return (
-        <div className="w-[80px] h-[80px] mx-auto mt-4 rounded-lg overflow-hidden transform transition-transform duration-300 hover:scale-110">
-            <img src="/src/assets/radio.jpg" alt="radio" className="w-full h-full object-cover" />
-        </div>
-    );
-}
